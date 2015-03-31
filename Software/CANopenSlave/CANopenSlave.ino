e4f142ce9264b82270860ae3211bc9362251c6af
Jens Geisler <jens.geisler@web.de>
2015-03-31 20:56:11 +0200

    removed wrong comment

--- Software/CANopenSlave/CANopenSlave.ino
+++ Software/CANopenSlave/CANopenSlave.ino
@@ -1,35 +1,3 @@
-/*
-This file is part of CanFestival, a library implementing CanOpen Stack.
-
-Copyright (C): Edouard TISSERANT and Francis DUPIN
-AVR Port: Andreas GLAUSER and Peter CHRISTEN
-
-See COPYING file for copyrights details.
-
-This library is free software; you can redistribute it and/or
-modify it under the terms of the GNU Lesser General Public
-License as published by the Free Software Foundation; either
-version 2.1 of the License, or (at your option) any later version.
-
-This library is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public
-License along with this library; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-*/
-/******************************************************************************
-Project description:
-Test projekt for a DS 401 slave, running on Atmel's STK500 with AT90CAN128
-Short description:
-  PORTA:	Inputs (Keys, low active)
-  PORTB:	Outputs (LEDs, low active)
-  PORTC:	Node ID (1 BCD switch, low active)
-
-******************************************************************************/
-
 #include <avr/io.h>
 #include "canfestival.h"
 #include "mcp_can.h"
